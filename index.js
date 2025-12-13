0bf3e0a9ed814d52f316be3e6b88f953eee5958e
WissemJderi <asqr43641@gmail.com>
2025-12-13 15:27:10 +0100

    remove specific CORS origin configuration for flexibility

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  }),
-);
+app.use(cors());
 
 let notes = [
   {
